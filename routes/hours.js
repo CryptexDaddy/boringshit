9d39b6a4396a94db4f3f8232be96227b07300c4d
y1n <>
2020-05-06 06:11:50 +0300

    Hours fix

--- routes/hours.js
+++ routes/hours.js
@@ -10,7 +10,9 @@ router.get('/', async function(req, res, next) {
     const days_arr = {};
     const selected_week = moment((req.user.time_windows.length ? req.user.time_windows.slice(-1)[0].shift_start : Date.now()) - 518400000).format('MMMM d, YYYY') + ' - ' + moment(req.user.time_windows.length ? req.user.time_windows.slice(-1)[0].shift_start : Date.now()).format('MMMM d, YYYY')
     for (let window of req.user.time_windows.filter(val => val.shift_start > moment().subtract('7', 'days').valueOf())) {
-        const total_time = (window.shift_end-window.shift_start - (window.lunch_end-window.lunch_start))/3600000
+        const work_time = (!window.shift_end ? Date.now() : window.shift_end) - window.shift_start
+        const lunch_time = (!window.lunch_end && window.lunch_start ? window.shift_end ? window.shift_end : Date.now() : window.lunch_end) - window.lunch_start
+        const total_time = (work_time - lunch_time)/3600000;
         if (days_arr[moment(window.shift_start).isoWeekday()]) days_arr[moment(window.shift_start).isoWeekday()]+=total_time
         else days_arr[moment(window.shift_start).isoWeekday()] = total_time
         // console.log(moment(window.shift_start - 518400000).format('dddd'));
@@ -20,3 +22,4 @@ router.get('/', async function(req, res, next) {
 });
 
 module.exports = router;
+
