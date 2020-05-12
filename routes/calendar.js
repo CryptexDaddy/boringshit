4469cda5502d27eb0bae9dfe5994cb26aad8f091
y1n <>
2020-05-12 20:12:07 +0300

    Big update!
    Calendar:
    - Current day has border
    - Days of other months have distinct bg
    - Current month string caption added
    - Tasks/Events restyled
    - Added margin-top to table below calendar
    
    Dashboard:
    - Fixed sizing, positioning
    - Increased size of active user item
    - Added "---" if no active users
    
    Hours:
    - Restyling to make elements more distinct
    - Fixed styling of overtime hours if its 0
    
    Live Chat:
    - Now fixed
    
    Side menu:
    - Now full width

--- routes/calendar.js
+++ routes/calendar.js
@@ -11,16 +11,18 @@ router.get('/', async function(req, res, next) {
     const first_sunday = weekday_of_first_month_day.day(0)
     const calendar = {};
     const events = await Event.find({}).exec()
+    const selected_month = moment().format('MMMM, YYYY')
     for (let x = 0; x < 42; x++) {
         const day_number = x > 0 ? moment(first_sunday.add(1, 'days')) : first_sunday;
         calendar[x] = {
+            extra: day_number.isSame(moment(), 'month') ? day_number.isSame(moment(), 'day') ? 2 : 1 : 0,
             day: day_number.format('D'), 
             tasks: await Task.find({$and: [{'time_alloted.task_end': {$gt: day_number.startOf('day').valueOf()}}, {'time_alloted.task_start': {$lt: day_number.endOf('day').valueOf()}}, {status: 1}]}).exec(),
             events: events.filter(val => val.time_alloted.task_end > day_number.startOf('day').valueOf() && val.time_alloted.task_start < day_number.endOf('day').valueOf())
         }
     }
     const users_doc = await User.find({status: 1}).exec();
-    res.render('calendar', { title: 'Calendar', active_users: users_doc, calendar, events });
+    res.render('calendar', { title: 'Calendar', active_users: users_doc, calendar, events, selected_month });
 });
 
 module.exports = router;
