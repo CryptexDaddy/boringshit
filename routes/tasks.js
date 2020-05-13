d10be60fb75de3c52e386665cdf341b7ca7ebe93
y1n <>
2020-05-13 06:24:16 +0300

    Admins can add people to tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -7,7 +7,8 @@ const {Company} = require('../models/company.model');
 /* GET home page. */
 router.get('/', async function(req, res, next) {
     const tasks = await Task.find({}).populate('employees').exec()
-    res.render('tasks', { title: 'Manage Tasks', tasks, page_description: `Here is what you are working on...` });
+    const team_members = req.user.company ? await User.find({'company': req.user.company._id}) : []
+    res.render('tasks', { title: 'Manage Tasks', tasks, page_description: `Here is what you are working on...`, team_members });
 });
 
 module.exports = router;
